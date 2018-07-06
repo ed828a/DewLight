a54caabac7bd6c91e073c9d04fd5230e6a7f42e6
Edward <ed828a@gmail.com>
2018-07-06 15:40:52 +0930

    update API

--- app/src/main/java/com/dew/edward/dewbe/util/Constants.kt
+++ app/src/main/java/com/dew/edward/dewbe/util/Constants.kt
@@ -1,5 +1,7 @@
 package com.dew.edward.dewbe.util
 
+import com.dew.edward.dewbe.BuildConfig
+
 
 /**
  * Created by Edward on 6/26/2018.
@@ -8,7 +10,7 @@ package com.dew.edward.dewbe.util
 const val YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/"
 const val NETWORK_PAGE_SIZE = 50  //Values must be within the range: [0, 50]
 const val PAGEDLIST_PAGE_SIZE = 15  // should be 20
-const val API_KEY = "AIzaSyA7cdJ8OPftCtkqIBpVuIX5CVtY7BW02JU"
+const val API_KEY = BuildConfig.YOUTUBE_API_KEY
 const val KEY_QUERY = "query"
 const val DEFAULT_QUERY = "trump"
 const val VIDEO_MODEL = "video_model"
