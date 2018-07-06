a6f8eb48ab0a7105eb53e07d81315d687e5cb3c3
Edward <ed828a@gmail.com>
2018-07-06 16:13:06 +0930

    update DewApp

--- app/src/main/java/com/dew/edward/dewbe/ui/DewApp.kt
+++ app/src/main/java/com/dew/edward/dewbe/ui/DewApp.kt
@@ -5,9 +5,6 @@ import android.content.SharedPreferences
 import android.support.multidex.MultiDexApplication
 import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
-import com.dew.edward.dewbe.util.KEY_DB_TYPE
-import com.dew.edward.dewbe.util.KEY_IS_DB_ENABLED
-import com.dew.edward.dewbe.util.KEY_PLAYER_TYPE
 import com.google.android.youtube.player.YouTubePlayer
 
 
@@ -30,9 +27,6 @@ class DewApp : MultiDexApplication() {
         super.onCreate()
         localBroadcastManager = LocalBroadcastManager.getInstance(applicationContext)
         sharedPreferences = getSharedPreferences("DewApp", Context.MODE_PRIVATE)
-        playerType = sharedPreferences.getString(KEY_PLAYER_TYPE, "ExoPlayer")
-        isDbEnabled = sharedPreferences.getBoolean(KEY_IS_DB_ENABLED, false)
-        dbType = sharedPreferences.getString(KEY_DB_TYPE, "DB_On_Disk")
 
         Log.d("DewApp", "playerType = $playerType, isDbEnabled = $isDbEnabled, dbType = $dbType")
     }
